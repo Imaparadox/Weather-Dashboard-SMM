3a467303b66ada4e2e1a0415c1bda5d6bfaca2de
Stephen Marsh <stevemarsh117@gmail.com>
2021-05-20 17:05:43 -0400

    created js global variables based off html classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,28 @@
-var test = $(".search-form-container");
-console.log(test);
+//Global Variables:
+//Search Elements
+var searchContainer = $(".search-form-container");
+var searchInput = $(".search-input");
+var searchButton = $(".search-btn");
+var clearHistoryButton = $(".clear-history-btn");
+var cityContainer = $(".city-container");
+var cityResults = $(".city-results");
+
+//Current Weather Elements
+var currentWeatherContainer = $(".current-weather-container");
+var cityNameTitle = $(".city-name-title");
+var currentWeatherIcon = $(".current-weather");
+var temperature = $(".temperature");
+var humidity = $(".humidity");
+var wind = $(".wind");
+var uvIndex = $(".uv-index");
+var weatherForecast = $(".weather-forecast");
+
+//Weather API key
+var aPI = "f1297039f6aa07eefba2bdda4c4c9199";
+
+//Function that fetches weather API from user input and search click
+
+//Function that creates div elements to city-results
+
+//Function that clears city-results when clicked
+
