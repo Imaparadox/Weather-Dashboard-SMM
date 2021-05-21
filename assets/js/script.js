406aaf591e4f78cc662e6b1298a867284174cc6c
Stephen Marsh <stevemarsh117@gmail.com>
2021-05-20 21:43:43 -0400

    Added fetch functions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,11 +18,42 @@ var uvIndex = $(".uv-index");
 var weatherForecast = $(".weather-forecast");
 
 //Weather API key
-var aPI = "f1297039f6aa07eefba2bdda4c4c9199";
+var apiKey = "f1297039f6aa07eefba2bdda4c4c9199";
 
 //Function that fetches weather API from user input and search click
+function searchResults(locationObject) {
+    var api = `https://api.openweathermap.org/data/2.5/onecall?lat=" + {lat} + "&lon=" + {lon} + "&exclude=" + {part} + "&appid=" + `;
+    var {lat} = locationObject
+    var {lon} = locationObject
+    fetch().then 
+
+};
+
+function getCoordinates(search) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}`;
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data.coord);
+        })
+}
+
+function handleSearch(event) {
+    event.preventDefault();
+    var search = searchInput.value.trim()
+    console.log(search);
+    getCoordinates(search);
+    searchInput.value = "";
+};
+
+searchButton.on("click", handleSearch);
 
 //Function that creates div elements to city-results
 
 //Function that clears city-results when clicked
 
+//Function that create
+
+//searchResults();
