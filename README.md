f0334f05183f51c01eec9f1a25732529f5664a6b
Stephen Marsh <stevemarsh117@gmail.com>
2021-05-28 16:30:44 -0400

    Added images into README.md

--- README.md
+++ README.md
@@ -4,33 +4,22 @@ This project is a homework assignment for Rutgers Coding Bootcamp.
 
 ## Deploy
 
-[github page site] !!!!!!!!!!!!!!!!!!!!!Enter the link here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+[github page site] https://imaparadox.github.io/Weather-Dashboard-SMM/
 
 ## Description 
 When the user searches for a city, they are presented with current and future conditions for that city. Consequently, the city entered is added to the search history. The user is presented with the city name, the date, an icon representing weather conditions, the temperature, the humidity, the wind speed, and the UV index. When the user views the UV index, the color changes depending on what the condition is (favorable, moderate, or severe). After the user enters a city in the input bar, a 5-day weather forecast is displayed, which lists the date, an icon (weather conditions), the temperature, the humidity, and the wind speed. Finally, the user can click on past-entered cities, which will display information of the selected city.
 
+## Without Content
+![ScreenShot](assets/images/Dashboard_without_info.png "ScreenShot")
 
-![ScreenShot]( "ScreenShot")
+## With Content
+![ScreenShot](assets/images/Dashboard_with_content.png "ScreenShot")
 
 ## Notes
-N/A
+I wasn't able to have the 5 day forecast be displayed whenever the user would click any of the list items for the city results. In addition, I wasn't able to figure out how to remove the certain appended elements, in which duplicate information would be displayed. I plan to fix this at a later date.
 
 ## Licenses
 N/A
 
 ## Acknowledgments for advice and assistance:
 N/A
-
-
-
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,106 @@
-//Global Variables:
-//Search Elements
-var searchContainer = $(".search-form-container");
-var searchInput = $(".search-input");
-var searchButton = $(".search-btn");
-var clearHistoryButton = $(".clear-history-btn");
-var cityContainer = $(".city-container");
-var cityResults = $(".city-results");
+$(document).ready(function () {
+    //Global Variables:
+    //Search Elements
+    var searchContainer = $(".search-form-container");
+    var searchInput = $(".search-input");
+    var searchButton = $(".search-btn");
+    var clearHistoryButton = $(".clear-history-btn");
+    var cityContainer = $(".city-container");
+    var cityResults = $(".city-results");
 
-//Current Weather Elements
-var currentWeatherContainer = $(".current-weather-container");
-var cityNameTitle = $(".city-name-title");
-var currentWeatherIcon = $(".current-weather-icon");
-var temperature = $(".temperature");
-var humidity = $(".humidity");
-var wind = $(".wind");
-var uvIndex = $(".uv-index");
-var weatherForecast = $(".weather-forecast");
+    //Current Weather Elements
+    var currentWeatherContainer = $(".current-weather-container");
+    var cityNameTitle = $(".city-name-title");
+    var currentWeatherIcon = $(".current-weather-icon");
+    var temperature = $(".temperature");
+    var humidity = $(".humidity");
+    var wind = $(".wind");
+    var uvIndex = $(".uv-index");
+    var weatherForecast = $(".weather-forecast");
 
-//Weather API key
-var apiKey = "f1297039f6aa07eefba2bdda4c4c9199";
+    //Weather API key
+    var apiKey = "f1297039f6aa07eefba2bdda4c4c9199";
 
-//Variable that stores city results
-var cityStorage = [];
+    //Variable that stores city results
+    var cityStorage = [];
 
-//Today's date
-var currentDate = moment().format("L");
-$(".current-date").text("(" + currentDate + ")");
+    //Today's date
+    var currentDate = moment().format("L");
+    $(".current-date").text("(" + currentDate + ")");
 
-//Fetches weather API from user input and search click
-function searchResults(coord) {
-    var { lat, lon } = coord;
-    var api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-    fetch(api)
-        .then(function (response) {
-            return response.json();
-        })
-        //Takes the data from fetch and displays the data in the web browser
-        .then(function (data) {
-            cityStorage.push(data)
-            console.log(data);
-            //Displays the five day forecast 
-            fiveDayForecast(data);
-            //Current weather is displayed in the browser window
-            temperature.text("Temperature: " + data.current.temp + " °F");
-            humidity.text("Humidity: " + data.current.humidity + " %");
-            wind.text("Wind Speed: " + data.current.wind_speed + " mph");
-            uvIndex.text("UV Index: " + data.current.uvi);
-            currentWeatherIcon.append($("<img>").attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png"))
-            //Changes the color of the text based on the current uv index
-            handleUvIndex(data);
-        });
-};
+    //Fetches weather API from user input and search click
+    function searchResults(coord) {
+        var { lat, lon } = coord;
+        var api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+        fetch(api)
+            .then(function (response) {
+                return response.json();
+            })
+            //Takes the data from fetch and displays the data in the web browser
+            .then(function (data) {
+                cityStorage.push(data)
+                console.log(data);
+                //Displays the five day forecast 
+                fiveDayForecast(data);
+                //Current weather is displayed in the browser window
+                temperature.text("Temperature: " + data.current.temp + " °F");
+                humidity.text("Humidity: " + data.current.humidity + " %");
+                wind.text("Wind Speed: " + data.current.wind_speed + " mph");
+                uvIndex.text("UV Index: " + data.current.uvi);
+                currentWeatherIcon.append($("<img>").attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png"))
+                //Changes the color of the text based on the current uv index
+                handleUvIndex(data);
+            });
+    };
 
-//Fetches city geographical locations
-function getCoordinates(search) {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}`;
-    fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            searchResults(data.coord);
-        });
-};
+    //.clear
 
-//Handles the search and calls the getCoordinates() function
-function handleSearch(cityName) {
-    //Trims empty spaces from user input
-    cityName.preventDefault();
-    var search = searchInput.val().trim();
-    console.log(search);
-    getCoordinates(search);
-    searchInput.val("");
-    //Calls the .on click function
-    historyClick(cityName)
-    //Creates city list elements and appends them  to city-results
-    var itemList = $('<li>').addClass('.city-results').text(search);
-    $(".city-results").append(itemList);
-    //Appends search result to city-name-title h3
-    $("#city-name-title").addClass('.city-name-title').text(search);
-    $(".city-name-title").append(cityNameTitle);
-    //Removes repeated cards
-    $(".weather-card-body").remove();
-    // $(currentWeatherIcon).remove();
+    //Fetches city geographical locations
+    function getCoordinates(search) {
+        var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}`;
+        fetch(apiUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                searchResults(data.coord);
+            });
+    };
 
-};
+    //Handles the search and calls the getCoordinates() function
+    function handleSearch(event) {
+        //Trims empty spaces from user input
+        event.preventDefault();
+        var search = searchInput.val().trim();
+        console.log(search);
+        getCoordinates(search);
+        searchInput.val("");
+        //Calls the .on click function
+        //historyClick(search)
+        addToStorage(search)
+        //Creates city list elements and appends them  to city-results
+        var itemList = $('<li>').addClass('.city-results').text(search);
+        itemList.attr("id", search);
+        $(".city-results").append(itemList);
+        //Appends search result to city-name-title h3
+        $("#city-name-title").text(search);
+        $(".city-name-title").append(cityNameTitle);
+        //Removes repeated cards
+        $(".weather-card-body").remove();
+        // $("#city-name-title").remove();
+        // $(.current-weather-icon).remove();
 
-//Click on the city result element to see storage
-function historyClick() {
-    $(".city-results").click(function () {
-        var clickEl = $(this).attr("id");
+    };
+
+ 
+    //Click on the city result element to see storage
+    $(".city-results").on("click", "li", function () {
+        var clickEl = $(this)[0].textContent;
+        $("#city-name-title").text(clickEl);
+        $(".weather-card-body").text(clickEl);
         console.log(clickEl);
-        // handleSearch(clickEl);
+        //fiveDayForecast();
+        getCoordinates(clickEl);
     });
     //Current Weather info
     // localStorage.setItem("city-current", JSON.stringify(data.current));
@@ -102,76 +111,88 @@ function historyClick() {
     // var dailyCityResult = JSON.parse(localStorage.getItem("city-daily"));
     // console.log(currentCityResult)
     // console.log(dailyCityResult)
-};
 
-//Calls the handleSearch()
-function historySearch(event) {
-    var city = event.target.text
-    handleSearch(city)
-    // historyClick(city)
-};
-
-//When search button is clicked the handleSearch function is called
-searchButton.on("click", handleSearch);
-//When clear history button is clicked the city list items are removed
-$(".clear-history-btn").click(function () { $("li").remove() });
-
-//Handles uv index
-function handleUvIndex(data) {
-    //Changes the class colors based on the uv rating 
-    uvIndex.addClass("badge");
-    if (data.current.uvi < 3) {
-        uvIndex.removeClass("bg-warning text-dark bg-danger")
-        uvIndex.addClass("bg-success");
-    } else if (data.current.uvi >= 3 && data.current.uvi < 6) {
-        uvIndex.removeClass("bg-success bg-danger");
-        uvIndex.addClass("bg-warning text-dark");
-    } else if (data.current.uvi >= 6 && data.current.uvi < 8) {
-        uvIndex.removeClass("bg-warning text-dark bg-success");
-        uvIndex.addClass("bg-danger");
-    } else {
-        uvIndex.removeClass("bg-warning text-dark bg-success");
-        uvIndex.addClass("bg-danger");
-    }
-};
-
-//Displays the five day fore cast for selected city 
-function fiveDayForecast(data) {
-    //Variable that stores the daily object
-    var fiveDayWeather = data.daily;
-    //For loop that will iterate 5 weather cards
-    for (var i = 1; i < 6; i++) {
-        console.log(fiveDayWeather[i]);
-        //Date for 5 day forecast
-        var fiveDayDaily = data.daily[i].dt;
-        var date = new Date(fiveDayDaily * 1000);
-        const day = date.getDate();
-        const month = date.getMonth() + 1;
-        const year = date.getFullYear();
-        // Query Selectors that will append divs with card classes to weatherForecast
-        //Card containers
-        var weatherForecastCol = $("<div class='col-12 col-md-6 col-lg mb-3'>");
-        var weatherForecastCard = $("<div class='weather-card text-dark bg-light'>");
-        var weatherForecastBody = $("<div class='weather-card-body'>");
-        //Card text
-        var weatherForecastDate = $("<h5 class='weather-card-title'>");
-        var weatherForecastIcon = $("<img>");
-        var weatherForecastTemp = $("<p class='weather-card-text mb-0'>");
-        var weatherForecastHumidity = $("<p class='weather-card-text mb-0'>");
-        //Append to containers
-        weatherForecast.append(weatherForecastCol);
-        weatherForecastCol.append(weatherForecastCard);
-        weatherForecastCard.append(weatherForecastBody);
-        //Append to text
-        weatherForecastBody.append(weatherForecastDate);
-        weatherForecastBody.append(weatherForecastIcon);
-        weatherForecastBody.append(weatherForecastTemp);
-        weatherForecastBody.append(weatherForecastHumidity);
-        //Entering info into cards 
-        weatherForecastDate.text(`${month}/${day}/${year}`);
-        weatherForecastIcon.attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
-        weatherForecastTemp.text("Temperature: " + data.daily[i].temp.day + " °F");
-        weatherForecastHumidity.text("Humidity: " + data.daily[i].humidity + " %");
+    function addToStorage() {
+        //push to cityStorage
+        localStorage.setItem('city-current', JSON.stringify(cityStorage));
     };
-};
+
+    //Calls the handleSearch()
+    function historySearch(event) {
+        var city = event.target.text
+        handleSearch(city)
+        //have a city history function that gets the global array from local storage
+        //and makes a <ul> with <li> for each city - make sure to add a class (searchedCities) and an id. I would suggest having the city name as the id
+        //Add a click event on searchedCities that grabs the id (city name)
+        //that would call your getCoordinates function to redo the search
+
+        // historyClick(city)
+    };
+
+    //When search button is clicked the handleSearch function is called
+    searchButton.on("click", handleSearch);
+    //cityResults.on("click", handleSearch);
+
+    //When clear history button is clicked the city list items are removed
+    $(".clear-history-btn").click(function () { $("li").remove() });
+
+    //Handles uv index
+    function handleUvIndex(data) {
+        //Changes the class colors based on the uv rating 
+        uvIndex.addClass("badge");
+        if (data.current.uvi < 3) {
+            uvIndex.removeClass("bg-warning text-dark bg-danger")
+            uvIndex.addClass("bg-success");
+        } else if (data.current.uvi >= 3 && data.current.uvi < 6) {
+            uvIndex.removeClass("bg-success bg-danger");
+            uvIndex.addClass("bg-warning text-dark");
+        } else if (data.current.uvi >= 6 && data.current.uvi < 8) {
+            uvIndex.removeClass("bg-warning text-dark bg-success");
+            uvIndex.addClass("bg-danger");
+        } else {
+            uvIndex.removeClass("bg-warning text-dark bg-success");
+            uvIndex.addClass("bg-danger");
+        }
+    };
+
+    //Displays the five day fore cast for selected city 
+    function fiveDayForecast(data) {
+        //Variable that stores the daily object
+        var fiveDayWeather = data.daily;
+        //For loop that will iterate 5 weather cards
+        for (var i = 1; i < 6; i++) {
+            console.log(fiveDayWeather[i]);
+            //Date for 5 day forecast
+            var fiveDayDaily = data.daily[i].dt;
+            var date = new Date(fiveDayDaily * 1000);
+            const day = date.getDate();
+            const month = date.getMonth() + 1;
+            const year = date.getFullYear();
+            // Query Selectors that will append divs with card classes to weatherForecast
+            //Card containers
+            var weatherForecastCol = $("<div class='col-12 col-md-6 col-lg mb-3'>");
+            var weatherForecastCard = $("<div class='weather-card text-dark bg-light'>");
+            var weatherForecastBody = $("<div class='weather-card-body'>");
+            //Card text
+            var weatherForecastDate = $("<h5 class='weather-card-title'>");
+            var weatherForecastIcon = $("<img>");
+            var weatherForecastTemp = $("<p class='weather-card-text mb-0'>");
+            var weatherForecastHumidity = $("<p class='weather-card-text mb-0'>");
+            //Append to containers
+            weatherForecast.append(weatherForecastCol);
+            weatherForecastCol.append(weatherForecastCard);
+            weatherForecastCard.append(weatherForecastBody);
+            //Append to text
+            weatherForecastBody.append(weatherForecastDate);
+            weatherForecastBody.append(weatherForecastIcon);
+            weatherForecastBody.append(weatherForecastTemp);
+            weatherForecastBody.append(weatherForecastHumidity);
+            //Entering info into cards 
+            weatherForecastDate.text(`${month}/${day}/${year}`);
+            weatherForecastIcon.attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
+            weatherForecastTemp.text("Temperature: " + data.daily[i].temp.day + " °F");
+            weatherForecastHumidity.text("Humidity: " + data.daily[i].humidity + " %");
+        };
+    };
+});
 
